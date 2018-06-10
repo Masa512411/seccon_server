bfabc9fd429677feed71c1b09864ac3bf80284a6
Masa512411 <masa1717@gol.com>
2018-06-10 17:54:13 +0900

    first

---
+++ get_agent.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+import os
+
+app = Flask(__name__)
+
+@app.route("/agent",methods=["POST"])
+def main():
+    return request.data
+
+if __name__ == "__main__":
+     port = int(os.environ.get("PORT",5000))
+         
+     app.run(debug=False,port=port,host='0.0.0.0')
+
