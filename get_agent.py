4ffcdb004d796aece24076b116fb71dc383b7baa
Masa512411 <masa1717@gol.com>
2018-06-10 20:36:28 +0900

    [add]

--- get_agent.py
+++ get_agent.py
@@ -1,18 +1,23 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 import os
 
 app = Flask(__name__)
 
-@app.route("/agent",methods=["POST"])
+@app.route("/agent",methods=["GET","POST"])
 def main():
-    return request.data
+    req = request.data
+    req = req.decode("utf-8")
+    with open("./templates/agent.html","w") as f:
+        string = "<p>{}</p>".format(str(req))
+        f.write(string)
+    return render_template("agent.html")
 
 @app.route("/check")
 def che():
-    return "<p> Hello</p>"
+    return render_template("agent.html")
 
 if __name__ == "__main__":
      port = int(os.environ.get("PORT",5000))
          
-     app.run(debug=False,port=port,host='0.0.0.0')
-
+#     app.run(debug=False,port=port,host='0.0.0.0')
+     app.run()
